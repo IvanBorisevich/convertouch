0be817bf8386fddf2205fd605fa788f2337838eb
johnbor7 <borisevich.ivan1994@gmail.com>
2023-11-04 22:52:24 +0600

    Support drag-n-drop (reorder) for conversion items list

--- lib/presentation/pages/items_view/conversion_items_view.dart
+++ lib/presentation/pages/items_view/conversion_items_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:convertouch/domain/model/unit_group_model.dart';
 import 'package:convertouch/domain/model/unit_value_model.dart';
 import 'package:convertouch/presentation/pages/items_view/item/item.dart';
@@ -27,41 +29,110 @@ class ConvertouchConversionItemsView extends StatefulWidget {
 
 class _ConvertouchConversionItemsViewState
     extends State<ConvertouchConversionItemsView> {
-  static const double _listSpacingLeftRight = 7;
+  static const double _listSpacingLeft = 1;
+  static const double _listSpacingRight = 7;
   static const double _listSpacingTop = 9;
-  static const double _listSpacingBottom = 8;
   static const double _listItemSpacing = 9;
+  static const double _dragHandlerWidth = 30;
+  static const double _dragHandlerHeight = 35;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsetsDirectional.fromSTEB(
-        _listSpacingLeftRight,
-        _listSpacingTop,
-        _listSpacingLeftRight,
-        _listSpacingBottom,
-      ),
-      itemBuilder: (context, index) {
-        UnitValueModel item = widget.convertedItems[index];
-        return ConvertouchItem.createItem(
-          item,
-          onTap: () {
-            widget.onItemTap?.call(item);
-          },
-          onValueChanged: (String value) {
-            widget.onItemValueChanged?.call(item, value);
-          },
-        ).buildForList();
-      },
-      separatorBuilder: (context, index) => Padding(
-        padding: EdgeInsetsDirectional.fromSTEB(
-          _listItemSpacing,
-          _listItemSpacing,
-          _listItemSpacing,
-          index == widget.convertedItems.length - 1 ? _listItemSpacing : 0,
+    final List<Card> cards = <Card>[
+      for (int index = 0; index < widget.convertedItems.length; index += 1)
+        Card(
+          key: Key('$index'),
+          margin: EdgeInsets.zero,
+          color: Colors.transparent,
+          shadowColor: Colors.transparent,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: _listItemSpacing),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: _dragHandlerWidth,
+                  height: _dragHandlerHeight,
+                  child: ReorderableDragStartListener(
+                    index: index,
+                    child: const Card(
+                      margin: EdgeInsets.zero,
+                      color: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      child: Icon(
+                        Icons.drag_indicator_outlined,
+                        color: Color(0xFF7FA0BE),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ConvertouchItem.createItem(
+                    widget.convertedItems[index],
+                    onTap: () {
+                      widget.onItemTap?.call(widget.convertedItems[index]);
+                    },
+                    onValueChanged: (String value) {
+                      widget.onItemValueChanged?.call(
+                        widget.convertedItems[index],
+                        value,
+                      );
+                    },
+                  ).buildForList(),
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
+    ];
+
+    /* animation when an item is dragged and dropped */
+    Widget proxyDecorator(
+      Widget child,
+      int index,
+      Animation<double> animation,
+    ) {
+      return AnimatedBuilder(
+        animation: animation,
+        builder: (BuildContext context, Widget? child) {
+          final double animValue = Curves.easeInOut.transform(animation.value);
+          final double elevation = lerpDouble(1, 6, animValue)!;
+          final double scale = lerpDouble(1, 1.02, animValue)!;
+          return Transform.scale(
+            scale: scale,
+            child: Card(
+              elevation: elevation,
+              shadowColor: Colors.transparent,
+              color: cards[index].color,
+              child: cards[index].child,
+            ),
+          );
+        },
+        child: child,
+      );
+    }
+
+    return ReorderableListView.builder(
+      itemBuilder: (context, index) {
+        return cards[index];
+      },
       itemCount: widget.convertedItems.length,
+      buildDefaultDragHandles: false,
+      proxyDecorator: proxyDecorator,
+      padding: const EdgeInsets.fromLTRB(
+        _listSpacingLeft,
+        _listSpacingTop,
+        _listSpacingRight,
+        0,
+      ),
+      onReorder: (int oldIndex, int newIndex) {
+        setState(() {
+          if (oldIndex < newIndex) {
+            newIndex -= 1;
+          }
+          final UnitValueModel item = widget.convertedItems.removeAt(oldIndex);
+          widget.convertedItems.insert(newIndex, item);
+        });
+      },
     );
   }
 }
