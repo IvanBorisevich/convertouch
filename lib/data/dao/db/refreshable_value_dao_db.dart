dccfd0d17c8df42114ab203af43a372ac310344b
johnbor7 <borisevich.ivan1994@gmail.com>
2023-12-26 22:31:44 +0600

    Prepare dao for refreshable values (e. g. units of temperature group)

--- lib/data/dao/db/refreshable_value_dao_db.dart
+++ lib/data/dao/db/refreshable_value_dao_db.dart
@@ -1,7 +1,15 @@
 import 'package:convertouch/data/dao/refreshable_value_dao.dart';
 import 'package:convertouch/data/entities/refreshable_value_entity.dart';
+import 'package:floor/floor.dart';
 
-class RefreshableValueDaoDb extends RefreshableValueDao {
+@dao
+abstract class RefreshableValueDaoDb extends RefreshableValueDao {
+  @override
+  Future<List<RefreshableValueEntity>> getList(List<int> unitIds) {
+    // TODO: implement getList
+    throw UnimplementedError();
+  }
+
   @override
   Future<int> insert(RefreshableValueEntity entity) {
     // TODO: implement insert
@@ -20,4 +28,9 @@ class RefreshableValueDaoDb extends RefreshableValueDao {
     throw UnimplementedError();
   }
 
+  @override
+  Future<List<int>> bulkMerge(List<RefreshableValueEntity> entity) {
+    // TODO: implement bulkMerge
+    throw UnimplementedError();
+  }
 }
--- lib/data/dao/refreshable_value_dao.dart
+++ lib/data/dao/refreshable_value_dao.dart
@@ -1,9 +1,13 @@
 import 'package:convertouch/data/entities/refreshable_value_entity.dart';
 
 abstract class RefreshableValueDao {
+  Future<List<RefreshableValueEntity>> getList(List<int> unitIds);
+
   Future<int> insert(RefreshableValueEntity entity);
 
   Future<int> update(RefreshableValueEntity entity);
 
   Future<int> merge(RefreshableValueEntity entity);
+
+  Future<List<int>> bulkMerge(List<RefreshableValueEntity> entity);
 }
