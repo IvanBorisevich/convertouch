4e617e2579ad18e266600bd3bb963edf3af78f49
johnbor7 <borisevich.ivan1994@gmail.com>
2023-12-29 09:06:06 +0600

    Update refreshing jobs ui

--- lib/domain/model/refreshing_job_model.dart
+++ lib/domain/model/refreshing_job_model.dart
@@ -5,16 +5,20 @@ import 'package:convertouch/domain/model/unit_group_model.dart';
 class RefreshingJobModel extends IdNameItemModel {
   final UnitGroupModel? unitGroup;
   final RefreshableDataPart dataRefreshType;
+  final bool isEnabled;
   final String cron;
   final String cronDescription;
+  final String lastRefreshTime;
 
   const RefreshingJobModel({
     required super.id,
     required super.name,
     required this.unitGroup,
     required this.dataRefreshType,
+    this.isEnabled = false,
     this.cron = defaultCron,
     this.cronDescription = defaultCronDescription,
+    this.lastRefreshTime = "-",
     super.itemType = ItemType.refreshingJob,
   });
 
@@ -24,7 +28,9 @@ class RefreshingJobModel extends IdNameItemModel {
     name,
     unitGroup,
     dataRefreshType,
+    isEnabled,
     cron,
+    lastRefreshTime,
     itemType,
   ];
 
--- lib/presentation/ui/items_view/item/refreshing_job_item.dart
+++ lib/presentation/ui/items_view/item/refreshing_job_item.dart
@@ -6,13 +6,15 @@ import 'package:flutter/material.dart';
 class ConvertouchRefreshingJobItem extends StatelessWidget {
   final RefreshingJobModel item;
   final double itemContainerHeight;
+  final double toggleButtonHeight;
   final double itemSpacing;
   final ListItemColorVariation color;
 
   const ConvertouchRefreshingJobItem(
     this.item, {
-    this.itemContainerHeight = 60,
-    this.itemSpacing = 8,
+    this.itemContainerHeight = 70,
+    this.toggleButtonHeight = 50,
+    this.itemSpacing = 7,
     required this.color,
     super.key,
   });
@@ -23,92 +25,101 @@ class ConvertouchRefreshingJobItem extends StatelessWidget {
       height: itemContainerHeight,
       child: Row(
         children: [
-          Padding(
-            padding: EdgeInsets.only(right: itemSpacing),
-            child: Container(
-              width: itemContainerHeight,
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
-                color: color.background,
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(
-                  color: color.border,
-                  width: 1,
-                ),
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.not_started_outlined),
-                onPressed: () {},
-              ),
-            ),
-          ),
           Expanded(
             child: Container(
               decoration: BoxDecoration(
                 color: color.background,
-                borderRadius: BorderRadius.circular(8),
+                borderRadius: BorderRadius.circular(10),
                 border: Border.all(
                   color: color.border,
                   width: 1,
                 ),
               ),
-              child: Column(
+              child: Row(
                 children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 9, top: 5),
-                      child: Text(
-                        item.name,
-                        style: TextStyle(
-                          fontFamily: quicksandFontFamily,
-                          fontWeight: FontWeight.w700,
-                          color: color.content,
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 12, top: 7),
+                            child: Text(
+                              item.name,
+                              style: TextStyle(
+                                fontFamily: quicksandFontFamily,
+                                fontWeight: FontWeight.w700,
+                                color: color.content,
+                                fontSize: 15,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                            ),
+                          ),
                         ),
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                      ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 3, left: 12),
+                            child: Text(
+                              'Last Refresh: ${item.lastRefreshTime}',
+                              style: TextStyle(
+                                fontFamily: quicksandFontFamily,
+                                fontWeight: FontWeight.w500,
+                                color: color.content,
+                                fontSize: 11,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 10, top: 4),
-                      child: Text(
-                        'Refreshable part: ${item.dataRefreshType}',
-                        style: TextStyle(
-                          fontFamily: quicksandFontFamily,
-                          fontWeight: FontWeight.w500,
-                          color: color.content,
-                          fontSize: 10,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                      ),
-                    ),
+                  _controlButton(
+                    icon: Icons.refresh_rounded,
                   ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text(
-                        'Cron: ${item.cronDescription}',
-                        style: TextStyle(
-                          fontFamily: quicksandFontFamily,
-                          fontWeight: FontWeight.w500,
-                          color: color.content,
-                          fontSize: 10,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                      ),
-                    ),
+                  _controlButton(
+                    icon: Icons.power_settings_new_outlined,
                   ),
                 ],
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _controlButton({
+    required IconData icon,
+    Function()? onClick,
+  }) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Padding(
+        padding: EdgeInsets.only(right: itemSpacing),
+        child: Container(
+          width: toggleButtonHeight,
+          height: toggleButtonHeight,
+          decoration: BoxDecoration(
+            color: color.background,
+            borderRadius: BorderRadius.circular(25),
+            border: Border.all(
+              color: color.border,
+              width: 1,
+            ),
+          ),
+          child: IconButton(
+            icon: Icon(
+              icon,
+              color: color.border,
+              size: 23,
+            ),
+            onPressed: onClick,
+          ),
+        ),
       ),
     );
   }
