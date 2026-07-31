152569f155f5995bfd29cfda7ee844fa521d8f53
johnbor7 <borisevich.ivan1994@gmail.com>
2026-07-31 12:03:51 +0500

    Update api host env variable, remove stub data from network dao

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,6 +1,11 @@
 import 'package:envied/envied.dart';
 part 'env.g.dart';
 
+/// Update env variables:
+/// 1) update .env
+/// 2) dart run build_runner clean
+/// 3) dart run build_runner build --delete-conflicting-outputs
+
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'API_HOST')
